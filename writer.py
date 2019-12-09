4e591fe352ad334077258b92fda70d6a77ae3c49
John Comonitski <58157753+JohnComonitski@users.noreply.github.com>
2019-12-09 13:29:45 -0500

    Update writer.py

--- writer.py
+++ writer.py
@@ -1,30 +1,62 @@
-from machine import SD
-import os
 import pycom
 import time
+import utime
+from encrypt import encrypt
+
 
 class writer:
-    def __init__(self, config, data_collector):
+    def __init__(self, config, data_collector, wlan, logger, encryption):
         self.sensor = data_collector
-        #Setting up SD Card
-        sd = SD()
-        sd.init()
-        os.mount(sd, '/sd')
+        self.Encryption = encryption
+        self.wifi = wlan
+        self.Logger = logger
+        self.Frequency = config["frequency"]
+
         #Preping to file
         f = open('/sd/moveData.txt', 'w')
         f.close()
 
     def runWriter(self):
+        self.wifi.disconnect()
+        Time = utime.localtime(None)
+        startTime = str(Time[1]) + "/"  + str(Time[2]) + "/" + str(Time[0]) + " at " + str(Time[3]) + ":" + str(Time[4]) + ":" + str(Time[5])
+        self.Logger.log("Session began at " + startTime)
+
+        pycom.rgbled(0x00FF00)  # Green
         while True:
-            #pycom.rgbled(0xFF0000)  # Red
-            time.sleep(.25)
+            time.sleep(1/self.Frequency)
 
-            angle = str(self.sensor.roll())
-            acceleration = self.sensor.acceleration()
-            currentTime = str(time.time())
+            self.sensor.checkBatt()
 
-            data = currentTime + "," + angle + "," + str(acceleration[0]) + "," + str(acceleration[1]) + "," + str(acceleration[2]) +"\n"
+            angle = str(self.sensor.getAngle())
+            coords = self.sensor.getGPS()
+            Time = utime.localtime(None)
+            currentTime = str(Time[1]) +"-"+ str(Time[2]) +"-"+ str(Time[0]) +"-" +str(Time[3])+":"+str(Time[4])+":"+str(Time[5])
+
+            data = currentTime + "," + angle + "," + str(coords[0]) + "," + str(coords[1])
             f = open('/sd/moveData.txt', 'a')
-            f.write(data)
+            f.write(data + "\n")
             f.close()
-            #pycom.rgbled(0x00FF00)  # Green
+
+    def runEWriter(self):
+        self.wifi.disconnect()
+        Time = utime.localtime(None)
+        startTime = str(Time[1]) + "/"  + str(Time[2]) + "/" + str(Time[0]) + " at " + str(Time[3]) + ":" + str(Time[4]) + ":" + str(Time[5])
+        self.Logger.log("Session began at " + startTime)
+
+        pycom.rgbled(0x00FF00)  # Green
+        while True:
+            time.sleep(1/self.Frequency)
+
+            self.sensor.checkBatt()
+
+            angle = str(self.sensor.getAngle())
+            coords = self.sensor.getGPS()
+            Time = utime.localtime(None)
+            currentTime = str(Time[1]) +"-"+ str(Time[2]) +"-"+ str(Time[0]) +"-" +str(Time[3])+":"+str(Time[4])+":"+str(Time[5])
+
+            data = currentTime + "," + angle + "," + str(coords[0]) + "," + str(coords[1])
+            msg = self.EncryptSion.RSAEncrypt(data)
+            f = open('/sd/EMoveData.txt', 'a')
+            f.write(msg + "\n")
+            f.close()
