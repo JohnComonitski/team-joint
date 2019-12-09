096fddddca398e63651a94be4498dea6220397f7
John Comonitski <58157753+JohnComonitski@users.noreply.github.com>
2019-12-09 13:23:42 -0500

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -1,21 +1,32 @@
+from crypto import AES
 import crypto
 
+
 class encrypt:
-    def __init__(self, config):
-        self.key = config["rijndael"]["publicKey"]
-        # Example of a JWT header + payload
-        self.header_payload = "eyJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ"
-        # Private key MUST BE in PKCS8 format !!!
-        self.f = open("cert/private_key_pkcs8.pem")
-        self.pk = f.read()
-        # Generate the signature
-        self.signature = crypto.generate_rsa_signature(header_payload, pk, pers="my_pers_string")
+    def __init__(self, config, logger):
+        f = open("/sd/public.pem")
+        self.pubKey = f.read()
+        self.Logger = logger
+        f.close()
 
-    def encrypt(plainText):
-        $ openssl genrsa -des3 -out private.pem 2048
-        # export the RSA public key to a file
-        $ openssl rsa -in private.pem -outform PEM -pubout -out public.pem
-        # export the RSA private key to a file
-        $ openssl rsa -in private.pem -out private_unencrypted.pem -outform PEM
+    def RSAEncrypt(self, plainText):
+        return crypto.rsa_encrypt(plainText, self.pubKey)
 
-        return crypto.rsa_encrypt(plainText, self.pk)
+    def RSADecrypt(self, privKey):
+        key = privKey.read()
+        privKey.close()
+        f = open('/sd/EMoveData.txt', 'r')
+        contents = f.readlines()
+        f.close()
+        f = open('/sd/decryptedMoveData.txt', 'w')
+        for l in contents:
+            try:
+                f.write(crypto.rsa_decrypt(l[:-1].encode('utf-8'), key) + "\n")
+                print(crypto.rsa_decrypt(l[:-1].encode('utf-8'), key))
+            except RuntimeError:
+                self.Logger.log("Error: line failed to decrypt")
+            except ValueError:
+                self.Logger.log("Error: private key error")
+
+        f.close()
+        return
